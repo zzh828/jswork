03665c533640d76a22a962fe4f3be9d499c709bc
apple <apple@jd505-26.local>
2019-10-14 11:37:19 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,13 +1,13 @@
 let str = '<table border="1">'
-for(let i=1 ;i<10;++i){
+for(let i=9 ;i>0;--i){
     str +='<tr>'
-    for(var j=1;j<=i;++j){
+    for(var j=i;j>0;--j){
         str+='<td>' +j +  '*'+ i+'='+(j*i)+'</td>'
     }
 str += '</tr>'
 }
 str += '</table>'
-document.getElementById('table1').innerHTML = str
+document.getElementById('table3').innerHTML = str
 
 str = '<table border="1">'
 for(let i =9;i>0;--i){
@@ -20,3 +20,16 @@ str +='</tr>'
 }
 str +='</table>'
 document.getElementById('table2').innerHTML = str
+
+str = '<table border="1">'
+for(let i =9;i>0;--i){
+str +='<tr>'
+for(var j=9; j>0;--j){
+    str +='<td>&nbsp;</td>'
+
+}
+str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table1').innerHTML = str
+
