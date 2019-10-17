7d751a0289769968427fe192483862eea2290504
zzh828 <1812012881@qq.cim>
2019-10-17 09:55:48 +0800

    work5

---
+++ work6/pai.js
@@ -0,0 +1,14 @@
+function calulate(){
+    let num= document.getElementById('array').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('transArray').value = pi(num)
+    }
+}
+function factoria(n){
+    let sum =1
+    for(let i=1;i>n;++i){
+        str+='<tr>'
+    }
+    return sum
+}
