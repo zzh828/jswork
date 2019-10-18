94f8d45c2e1562e2cd4b42ee4620c22e6dc60a45
zzh828 <771479513@qq.com>
2019-10-18 10:08:16 +0800

    work5

--- work6/pai.js
+++ work6/pai.js
@@ -1,14 +1,32 @@
-function calulate(){
-    let num= document.getElementById('array').value
+function calculate(){
+    let num= document.getElementById('num').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById('transArray').value = pi(num)
+        document.getElementById('pai').value = pi(num)
     }
 }
-function factoria(n){
+function factorial(n){
     let sum =1
-    for(let i=1;i>n;++i){
-        str+='<tr>'
+    for(let i=1;i<=n;++i){
+           sum *=i
     }
     return sum
 }
+function oddFactorial(n){
+    let sum =1
+    for(let i=1;i<=n;++i ){
+     sum *=2*i+1
+    }
+   return sum
+}
+function pi(num){
+    let sum = 0
+    for (let i=0;i<=num; ++i){
+        let dividend = factorial(i)
+     let divisor = oddFactorial(i)
+     sum += (dividend/divisor)
+    }
+    return sum*2
+}
+
+calculate()
