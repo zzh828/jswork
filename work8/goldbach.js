c59c015887a2ffd6d31258b2d0e1f402d66ce97c
zzh828 <771479513@qq.com>
2019-10-25 14:43:21 +0800

    rwork8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,24 +1,29 @@
-
-
-function goldbach(a){
-var sum=0;
-for(var i=1;i<=a;i++){
-if(a % i ==0){
-                sum++;
+function goldbach() {
+    var value = document.getElementById('odd').value
+    if(value%2!==0){
+        alert("请输入偶数")
+    }
+    let goldbach = document.getElementById('goldbach')
+    var arr =[]
+    var a = 0;
+    for(var i=2;i<=value;i++){
+        a = 0
+        for(var j=2;j<i;j++){
+            if(i%j==0){
+                a++
             }
         }
-if(sum==2){
-return true;
-        }else{
-return false;
+        if(a==0){
+            arr.push(i)
         }
     }
-
-function isTrue(a){
-for(var i=2;i<a-2;i++){
-var j = a -i;
-if(goldbach(i)&&goldbach(j)){
-    document.getElementById(a+"可以拆分为两个质数"+ i+"与"+j+"的和");
-        }
-      }
+    var str = ''
+    for(let i=0;i<(arr.length)/2;i++){
+       for(let j=0;j<arr.length;j++){
+           if((arr[i]+arr[j])===Number(value)){
+               str += '<div>'+value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+ '</div>'
+           }
+       }
     }
+    goldbach.innerHTML = str
+}
