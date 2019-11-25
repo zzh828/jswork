34f24cee04d0370c674d2f7d7d8d986440b6e8d6
zzh828 <771479513@qq.com>
2019-11-25 14:39:26 +0800

    work12

--- work12/calendar.js
+++ work12/calendar.js
@@ -1,7 +1,7 @@
 function calendar(y){
     var w= new Date(y,0).getDay();
 var html='<div class="box">'
-for(var m=i;m<=12;++m){
+for(var m=1;m<=12;++m){
     html +='<table>';
     html+='<tr class="title"><th colspan="7">'+y+'年'+m+'月</th></tr>';
     html +='<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>'
